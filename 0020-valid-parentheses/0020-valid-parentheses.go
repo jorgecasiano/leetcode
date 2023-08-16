9854f588f5e8902fd121b6a91ac7251271b6a7ec
Jorge Casiano <kas7ux@gmail.com>
2023-08-16 19:51:10 +0200

    Time: 1 ms (72.25%), Space: 2 MB (28.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.go
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -1,3 +1,39 @@
+type (
+	Stack struct {
+		top *node
+		length int
+	}
+	node struct {
+		value interface{}
+		prev *node
+	}	
+)
+// Create a new stack
+func New() *Stack {
+	return &Stack{nil,0}
+}
+// Return the number of items in the stack
+func (this *Stack) Len() int {
+	return this.length
+}
+// Pop the top item of the stack and return it
+func (this *Stack) Pop() interface{} {
+	if this.length == 0 {
+		return nil
+	}
+	
+	n := this.top
+	this.top = n.prev
+	this.length--
+	return n.value
+}
+// Push a value onto the top of the stack
+func (this *Stack) Push(value interface{}) {
+	n := &node{value,this.top}
+	this.top = n
+	this.length++
+}
+
 const symbols = "[({])}"
 
 func isValid(s string) bool {
@@ -13,25 +49,28 @@ func isValid(s string) bool {
 		symbols[5]: 2,
 	}
 
-	var stack []byte
+	var stack = New()
 
 	for i := 0; i < len(s); i++ {
 		val, found := closeSymbols[s[i]]
 		if found {
-			if len(stack) == 0 || openSymbols[stack[len(stack)-1]] != val {
+			if stack.Len() == 0 {
 				return false
 			}
 
-			stack = stack[:len(stack)-1]
+			curr := stack.Pop().(byte)
+			if openSymbols[curr] != val {
+				return false
+			}
 		} else {
 			_, found = openSymbols[s[i]]
 			if found {
-				stack = append(stack, s[i])
+				stack.Push(s[i])
 			} else {
 				return false
 			}
 		}
 	}
 
-	return len(stack) == 0
+	return stack.Len() == 0
 }
